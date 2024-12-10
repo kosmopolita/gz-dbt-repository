280729af7bf48939d24136bb603055088e509097
Rafael Rotter Meda <rafaelmeda@gmail.com>
2024-12-10 19:48:41 +0000

    Config to materialized of finance_days

---
+++ models/mart/finance/finance_days.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table')}}
+
+SELECT
+    date_date,
+    COUNT(orders_id) AS nb_transactions,
+    ROUND(SUM(revenue), 2) AS revenue,
+    ROUND(SUM(revenue) / COUNT(orders_id), 2) AS average_basket,
+    ROUND(SUM(margin), 2) AS margin,
+    ROUND(SUM(operation_margin), 2) AS operation_margin,
+    ROUND(SUM(purchase_cost), 2) AS purchase_cost,
+    ROUND(SUM(shipping_fee), 2) AS shipping_fee,
+    ROUND(SUM(log_cost), 2) AS log_cost,
+    SUM(quantity) AS quantity,
+FROM {{ ref('int_orders_operational')}}
+GROUP BY date_date
+ORDER BY date_date DESC
